99bd3eaa7f2355b5982986788cb7f261bc939b6c
arkady <arkadychakhalyan@gmail.com>
2025-05-04 13:32:07 +0300

    cc21

--- src/cashback/cashback.service.ts
+++ src/cashback/cashback.service.ts
@@ -6,10 +6,7 @@ import { CreateCashbackDto } from './createCashbackDto';
 import { TCashbackId, TUserId } from 'cashback-check-types';
 import { getCashbackIcon } from './utils/getCashbackIcon';
 import { getCashbackColor } from './utils/getCashbackColor';
-import { getCashbackNextMonth } from './utils/getCashbackNextMonth';
 import { getIsCashbackExpired } from './utils/getIsCashbackExpired';
-import { getCashbackBankOrderNumber } from './utils/getCashbackBankOrderNumber';
-import { getCashbackCardOrderNumber } from './utils/getCashbackCardOrderNumber';
 
 @Injectable()
 export class CashbackService {
@@ -39,132 +36,7 @@ export class CashbackService {
         }
     }
 
-    async addCardOrderNumberIfNeeded(userId: TUserId) {
-        const cashbacks  = await this.cashbackModel
-            .find({ userId })
-            .lean()
-            .exec();
-        for (const cashback of cashbacks) {
-            if (Number.isFinite(cashback.cardOrderNumber)) continue;
-            const periodCashbacks = [];
-            const cashbackDate = new Date(cashback.timestamp);
-            cashbacks.forEach(item => {
-                const itemDate = new Date(item.timestamp);
-                if (
-                    cashback._id !== item._id &&
-                    itemDate.getFullYear() === cashbackDate.getFullYear() &&
-                    itemDate.getMonth() === cashbackDate.getMonth()
-                ) {
-                    periodCashbacks.push(item);
-                }
-            });
-            const cardOrderNumber = getCashbackCardOrderNumber(periodCashbacks, cashback.card, cashback.bank);
-            const index = cashbacks.findIndex(item => item._id.toString() === cashback._id.toString());
-            if (index !== -1) {
-                cashbacks[index] = {
-                    ...cashback,
-                    cardOrderNumber,
-                };
-            }
-            await this.update(cashback._id.toString(), { cardOrderNumber });
-        }
-    }
-
-    async addTimelessCashbacks(userId: TUserId) {
-        const cashbacks = await this.cashbackModel
-            .find({ userId })
-            .lean()
-            .exec();
-
-        for (const cashback of cashbacks) {
-            if (!cashback.limitless) continue;
-
-            const { timestamp } = cashback;
-            const currentMonth = new Date().getMonth();
-            const cashbackMonth = new Date(timestamp).getMonth();
-            if (!getIsCashbackExpired(timestamp) && cashbackMonth > currentMonth) return;
-
-            const currentCashbacks = cashbacks.filter(item => {
-                return item.name === cashback.name &&
-                    item.bank === cashback.bank &&
-                    item.percentage === cashback.percentage &&
-                    item?.card?.name === cashback?.card?.name;
-            });
-
-            let isCurrentMonth = false;
-            let isNextMonth = false;
-            for (let i = 0; i < currentCashbacks.length; i++) {
-                const timestamp = currentCashbacks[i].timestamp;
-                if (getIsCashbackExpired(timestamp)) return;
-
-                const cashbackMonth = new Date(timestamp).getMonth();
-                const nextMonth = getCashbackNextMonth();
-                const currentMonth = new Date().getMonth();
-
-                if (cashbackMonth === currentMonth) {
-                    isCurrentMonth = true;
-                } else if (cashbackMonth === nextMonth) {
-                    isNextMonth = true;
-                }
-
-                if (isCurrentMonth && isNextMonth) {
-                    break;
-                }
-            }
-
-            if (!isCurrentMonth) {
-                const currentMonthCashbacks = cashbacks.filter(cashback => {
-                    const timestamp = cashback.timestamp;
-                    const cashbackMonth = new Date(timestamp).getMonth();
-                    const currentMonth = new Date().getMonth();
-                    return getIsCashbackExpired(timestamp) && cashbackMonth === currentMonth;
-                });
-                const newCashback = await this.create({
-                    userId,
-                    card: cashback.card,
-                    bank: cashback.bank,
-                    icon: cashback.icon,
-                    color: cashback.color,
-                    name: cashback.name,
-                    percentage: cashback.percentage,
-                    orderNumber: currentMonthCashbacks.length,
-                    bankOrderNumber: getCashbackBankOrderNumber(currentMonthCashbacks as Cashback[], cashback.bank),
-                    cardOrderNumber: getCashbackCardOrderNumber(currentMonthCashbacks as Cashback[], cashback.card, cashback.bank),
-                    timestamp: Date.now(),
-                });
-                cashbacks.push(newCashback as any);
-            }
-
-            if (!isNextMonth) {
-                const nextMonthCashbacks = cashbacks.filter(cashback => {
-                    const timestamp = cashback.timestamp;
-                    const cashbackMonth = new Date(timestamp).getMonth();
-                    const nextMonth = getCashbackNextMonth();
-                    return getIsCashbackExpired(timestamp) && cashbackMonth === nextMonth;
-                });
-                const date = new Date();
-                date.setMonth(date.getMonth() + 1);
-                const newCashback = await this.create({
-                    userId,
-                    card: cashback.card,
-                    bank: cashback.bank,
-                    icon: cashback.icon,
-                    color: cashback.color,
-                    name: cashback.name,
-                    percentage: cashback.percentage,
-                    orderNumber: nextMonthCashbacks.length,
-                    bankOrderNumber: getCashbackBankOrderNumber(nextMonthCashbacks as Cashback[], cashback.bank),
-                    cardOrderNumber: getCashbackCardOrderNumber(nextMonthCashbacks as Cashback[], cashback.card, cashback.bank),
-                    timestamp: date.getTime(),
-                });
-                cashbacks.push(newCashback as any);
-            }
-        }
-    }
-
     async findAll(userId: TUserId): Promise<Cashback[]> {
-        await this.addCardOrderNumberIfNeeded(userId);
-        await this.addTimelessCashbacks(userId);
         await this.removeOldCashbacks(userId);
         return await this.cashbackModel.find({ userId }).exec();
     }
--- src/cashback/utils/getCashbackNextMonth.ts
+++
@@ -1,5 +0,0 @@
-export function getCashbackNextMonth() {
-    const date = new Date();
-    date.setMonth(new Date().getMonth() + 1);
-    return date.getMonth();
-}

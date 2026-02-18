d1b616b902c80e1c45cf7ec1f4276a3cfc711e61
arkady <arkadychakhalyan@gmail.com>
2026-02-18 20:06:58 +0300

    release-1.2.9

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
         origin: process.env.CLIENT_URL,
+        credentials: true,
     });
     await app.listen(process.env.PORT ?? 3000);
 }

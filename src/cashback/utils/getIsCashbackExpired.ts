f18031450036b4c157652af723502e00168c78af
arkady <arkadychakhalyan@gmail.com>
2026-03-28 16:22:52 +0300

    Fix date handling in getIsCashbackExpired function to use UTC methods

--- src/cashback/utils/getIsCashbackExpired.ts
+++ src/cashback/utils/getIsCashbackExpired.ts
@@ -2,8 +2,8 @@ export function getIsCashbackExpired(timestamp: number) {
     const date = new Date(timestamp);
     const currentMonthStart = new Date();
 
-    currentMonthStart.setDate(1);
-    currentMonthStart.setHours(0, 0, 0, 0);
+    currentMonthStart.setUTCDate(1);
+    currentMonthStart.setUTCHours(0, 0, 0, 0);
 
     return date < currentMonthStart;
 }
